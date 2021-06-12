d9301bbff7f9e904558833a23b39304a78be6bfe
saattrupdan <saattrupdan@gmail.com>
2021-06-12 14:26:25 +0100

    feat: Include more filters on the sentences. docs: Add more comments

--- src/andersen_crawler.py
+++ src/andersen_crawler.py
@@ -1,3 +1,5 @@
+'''Script that scrapes HC Andersen fairytales'''
+
 import requests
 from bs4 import BeautifulSoup
 from newspaper import Article
@@ -6,31 +8,64 @@ from tqdm.auto import tqdm
 from pathlib import Path
 
 
-base_url = 'https://www.andersenstories.com/da/andersen_fortaellinger/'
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
-html = requests.get(base_url + 'list', headers=headers)
-soup = BeautifulSoup(html.content, 'html.parser')
-ul = soup.find('ul', class_='bluelink')
-links = [base_url + a['href'] for a in ul.find_all('a', href=True)]
+def scrape_andersen():
+    '''Scrape HC Andersen fairytales and store them in `data/output.txt`'''
 
-path = Path('data') / 'output.txt'
-if not path.exists():
-    path.touch()
+    # Get the HTML from the website containing all the fairytales
+    base_url = 'https://www.andersenstories.com/da/andersen_fortaellinger/'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                             'AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/83.0.4103.61 Safari/537.36'}
+    html = requests.get(base_url + 'list', headers=headers)
 
-for link in tqdm(links):
-    article = Article(link)
-    article.download()
-    article.parse()
-    doc = article.text
-    doc = re.sub('"', '', doc)
-    doc = re.sub('! *', '!\n', doc)
-    doc = re.sub(r'[:;.] *', '.\n', doc)
-    doc = re.sub('\n+', '\n', doc)
-    lines = [line for line in doc.split('\n')
-                  if len(line.split()) >= 4 and
-                     len(line.split()) < 14 and
-                     len(re.sub('[^a-z]', '', line)) > 0]
-    doc = '\n'.join(lines)
+    # Parse the HTML and extract all the links for the individual fairytales
+    soup = BeautifulSoup(html.content, 'html.parser')
+    ul = soup.find('ul', class_='bluelink')
+    links = [base_url + a['href'] for a in ul.find_all('a', href=True)]
 
-    with path.open('a') as f:
-        f.write(doc + '\n')
+    # Create the output file if it does not already exist
+    path = Path('data') / 'output.txt'
+    if not path.exists():
+        path.touch()
+
+    # Scrape all fairytales
+    for link in tqdm(links):
+
+        # Download the fairytale and extract the text, using the `newspaper3k`
+        # library
+        article = Article(link)
+        article.download()
+        article.parse()
+        doc = article.text
+
+        # Remove numbers, and split into sentences
+        doc = re.sub('["0-9]', '', doc)
+        doc = re.sub('! *', '!\n', doc)
+        doc = re.sub(r'[:;.] *', '.\n', doc)
+
+        # Remove duplicate line breaks
+        doc = re.sub('\n+', '\n', doc)
+
+        # Split into lines, and ensure that the lines has between 4 and 14
+        # words, as that's the requirement by CommonVoice. Altså ensure that
+        # the sentences are in title case
+        lines = [line.title() for line in doc.split('\n')
+                      if len(line.split()) >= 4 and len(line.split()) < 14]
+
+        # Filter the lines further by removing all the sentences starting with
+        # 'sagde' and 'råbte', and removing sentences which are just symbols
+        lines = [line for line in lines
+                      if len(re.sub('[^a-z]', '', line)) > 0 and
+                         not line.lower().startswith('sagde') and
+                         not line.lower().startswith('råbte')]
+
+        # Join all the sentences into one long string
+        doc = '\n'.join(lines)
+
+        # Append the string to the output file
+        with path.open('a') as f:
+            f.write(doc + '\n')
+
+
+if __name__ == '__main__':
+    scrape_andersen()
